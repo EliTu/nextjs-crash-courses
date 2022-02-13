349a822dab77930528f203f39c4f262fbc6578ac
Eliad Tuksher <eliad91@gmail.com>
2022-02-13 12:18:50 +0200

    dynamic routing

---
+++ pages/news/[newsId].tsx
@@ -0,0 +1,3 @@
+export default function NewsItem() {
+  return <div>News Item nested route!</div>;
+}
--- pages/news/something-important.tsx
+++
@@ -1,3 +0,0 @@
-export default function SomethingImportant() {
-  return <div>Something important nested route!</div>;
-}

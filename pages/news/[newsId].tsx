860173f7fc2eda36adbe139c4879e1b875462ab1
Eliad Tuksher <eliad91@gmail.com>
2022-02-13 12:26:14 +0200

    getting the dynamic route id

--- pages/news/[newsId].tsx
+++ pages/news/[newsId].tsx
@@ -1,3 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function NewsItem() {
-  return <div>News Item nested route!</div>;
+  const { query } = useRouter();
+
+  return (
+    <>
+      <h1>News Item nested route</h1>
+      <div>id: {query.newsId}</div>
+    </>
+  );
 }

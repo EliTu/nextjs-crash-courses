ef7edb89c9bc58a79c7ac296a49f33f9ed4da0f6
Eliad Tuksher <eliad91@gmail.com>
2022-02-18 14:10:52 +0200

    working with Head C

--- Traversy/traversy-media-crash-course/pages/index.tsx
+++ Traversy/traversy-media-crash-course/pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 export default function Home({}: NextPage) {
   return (
     <div>
+      <Head>
+        <title>WebDev News</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
       <h1>Welcome!</h1>
     </div>
   );

af42a182e5dbb1c6a1289efce25189a0b79209a8
Eliad Tuksher <eliad91@gmail.com>
2022-02-18 13:34:17 +0200

    successful build

--- Accademind/course-project/next.config.js
+++ Accademind/course-project/next.config.js
@@ -1,32 +1,6 @@
-<<<<<<< HEAD
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: 'maximilian',
-        mongodb_password: '2YkcXq43KyPk0vqp',
-        mongodb_clustername: 'cluster0',
-        mongodb_database: 'my-site-dev',
-      },
-    };
-  }
-
-  return {
-    env: {
-      mongodb_username: 'maximilian',
-      mongodb_password: '2YkcXq43KyPk0vqp',
-      mongodb_clustername: 'cluster0',
-      mongodb_database: 'my-site',
-    },
-  };
-};
-=======
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
->>>>>>> zz-prj-nextjs-summary
+module.exports = nextConfig;

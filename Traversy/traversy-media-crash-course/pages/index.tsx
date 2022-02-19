65b6d42ae0fee0bbf5d1a3596e4ff3452dbe0300
Eliad Tuksher <eliad91@gmail.com>
2022-02-19 16:03:10 +0200

    add getStaticProps and fetch articles

--- Traversy/traversy-media-crash-course/pages/index.tsx
+++ Traversy/traversy-media-crash-course/pages/index.tsx
@@ -1,14 +1,48 @@
-import type { NextPage } from "next";
+import type { GetStaticPropsContext, GetStaticPropsResult } from "next";
 import Head from "next/head";
 
-export default function Home({}: NextPage) {
+interface Article {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface HomeProps {
+  articles: Article[];
+}
+
+export default function Home({ articles }: HomeProps) {
+  console.log(articles);
+
   return (
     <div>
       <Head>
         <title>WebDev News</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Welcome!</h1>
+      {articles.map((article) => (
+        <h3>{article.title}</h3>
+      ))}
     </div>
   );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext): Promise<GetStaticPropsResult<HomeProps>> {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=6");
+    const articles = await res.json();
+
+    return {
+      props: {
+        articles,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    // in case of error, retuning an object with the notFound property will result in redirection to the 404 page
+    return {
+      notFound: true,
+    };
+  }
 }

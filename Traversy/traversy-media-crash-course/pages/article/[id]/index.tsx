0f16c72ad75265e0132e5487b0b12187fe1d55f7
Eliad Tuksher <eliad91@gmail.com>
2022-02-20 17:19:24 +0200

    get dynamic data with getServerSideProps

--- Traversy/traversy-media-crash-course/pages/article/[id]/index.tsx
+++ Traversy/traversy-media-crash-course/pages/article/[id]/index.tsx
@@ -1,3 +1,37 @@
-export default function ArticleDetails() {
-	return <div>This is article</div>;
+import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import Link from 'next/link';
+import { Article } from '../..';
+
+interface ArticleDetailsProps {
+	article: Article;
+}
+
+export default function ArticleDetails({ article }: ArticleDetailsProps) {
+	return (
+		<>
+			<h1>{article.title}</h1>
+			<p>{article.body}</p>
+			<br />
+			<Link href="/">Go back</Link>
+		</>
+	);
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<{}>> {
+	try {
+		const { params } = context;
+		const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params?.id}`);
+		const article = await res.json();
+
+		return {
+			props: {
+				article,
+			},
+		};
+	} catch (error) {
+		console.log(error);
+		return {
+			notFound: true,
+		};
+	}
 }
